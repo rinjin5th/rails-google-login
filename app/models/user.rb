b95d7383f7a790dab3da0f2b5b006cf7f754dcc1
iguwo <imasa510@gmail.com>
2018-06-16 17:08:36 +0900

    googleログイン実装

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :trackable, :omniauthable, omniauth_providers: %i(google)
+
+  protected
+  def self.find_for_google(auth)
+    user = User.find_by(email: auth.info.email)
+
+    unless user
+      user = User.create(email: auth.info.email,
+                         provider: auth.provider,
+                         uid: auth.uid,
+                         token: auth.credentials.token,
+                         encrypted_password: Devise.friendly_token[0, 20])
+    end
+    user
+  end
+end
